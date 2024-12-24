950ae57c384fff6b6d1f40c8ff269f82364a7013
pouriaco <p.pouriaco@gmail.com>
2024-12-25 01:35:23 +0330

    fix conflicts in branch

--- warehouse-frontend/src/app/app-routing.module.ts
+++ warehouse-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
+import { SidebarComponent } from './components/nav/sidebar/sidebar.component';
 
 const routes: Routes = [
   // {path:'' , component:AppComponent},
-  {path:'login' , component:LoginComponent}
+  {path:'login' , component:LoginComponent},
+  {path:'sidebar' , component:SidebarComponent}
 ];
 
 @NgModule({
--- warehouse-frontend/src/app/app.module.ts
+++ warehouse-frontend/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
+import { SidebarComponent } from './components/nav/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
