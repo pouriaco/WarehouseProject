c113cb68c929b085aabb59b6c98007bb39f13d8a
mminaii247@gmail.com <mminaii247@gmail.com>
2025-01-01 18:40:01 +0330

    CRUDS in 2025/1/1

--- Jebheye Moqavemat C#/Application/Dtos/WarehouseDtos/CRUDWarehouseDTO.cs
+++ Jebheye Moqavemat C#/Application/Dtos/WarehouseDtos/CRUDWarehouseDTO.cs
@@ -5,7 +5,6 @@ namespace Application.Dtos.WarehouseDtos
 {
     public class CRUDWarehouseDTO
     {
-        public int Id { get; set; } // برای عملیات آپدیت و حذف
         public string Name { get; set; } // نام انبار
         public double Area { get; set; } // مساحت انبار
         public int CityId { get; set; } // شناسه شهر
--- Jebheye Moqavemat C#/Domain/Interfaces/IWarehouses/IWarehouseRepository.cs
+++ Jebheye Moqavemat C#/Domain/Interfaces/IWarehouses/IWarehouseRepository.cs
@@ -14,14 +14,14 @@ namespace Domain.Interfaces.IWarehouses
       /* U */ public void UpdateWarehouse(WarehouseEntity warehouse);
       /* D */ public void DeleteWarehouse(int warehouseId);
 
+           //
+              public List<WarehouseEntity> GetAllWarehouses();
+           //
+              public double CheckWarehouseCapacity(int warehouseId); // بررسی و محاسبه ظرفیت باقی‌مانده یک انبار
+              public IEnumerable<SerialEntity> GetWarehouseInventoryReport(int warehouseId); // گزارش موجودی انبار شامل لیستی از محصولات و تعداد آن‌ها
 
-        //
-        public List<WarehouseEntity> GetAllWarehouses();
-        //
-
-
-        public double CheckWarehouseCapacity(int warehouseId); // بررسی و محاسبه ظرفیت باقی‌مانده یک انبار
-       public IEnumerable<SerialEntity> GetWarehouseInventoryReport(int warehouseId); // گزارش موجودی انبار شامل لیستی از محصولات و تعداد آن‌ها
-      // public bool TransferProduct(int serialNumber, int fromWarehouseId, int toWarehouseId , int forShelfId , int toShelfId); // انتقال کالا از یک انبار به انبار دیگر
     }
 }
+
+
+// public bool TransferProduct(int serialNumber, int fromWarehouseId, int toWarehouseId , int forShelfId , int toShelfId); // انتقال کالا از یک انبار به انبار دیگر
